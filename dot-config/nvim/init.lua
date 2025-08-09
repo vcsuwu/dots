6498f027889c1e2a31fe484b2125affdf2e62468
vocus <zenyabtw@gmail.com>
2025-08-09 15:47:00 +0500

    some changes?

---
+++ dot-config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+vim.opt.number = true             
+vim.opt.relativenumber = true 
+vim.opt.expandtab = true 
+vim.opt.tabstop = 2            
+vim.opt.shiftwidth = 2 
+vim.opt.autoindent = true
+vim.opt.smartindent = true        
+vim.opt.wrap = false              
+vim.opt.termguicolors = true 
